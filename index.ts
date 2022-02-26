7df580b6333d28b16681804d2a1ae61663a48b09
Daniel Poles <daniel@dpulm.online>
2022-02-26 04:22:52 +0100

    main script

---
+++ index.ts
@@ -0,0 +1,39 @@
+import 'dotenv/config';
+
+// import { auth, xadd } from '@upstash/redis'
+import IORedis from 'ioredis';
+
+import { promisify } from 'util';
+import { join } from 'path';
+const execFile = promisify(require('child_process').execFile);
+
+// auth(process.env['UPSTASH_REDIS_REST_URL'], process.env['UPSTASH_REDIS_REST_TOKEN']);
+
+class Reader {
+    static async readData() {
+        const { stdout } = await execFile(join(__dirname, 'sensor'));
+        return JSON.parse(stdout);
+    }
+}
+
+class Transmitter {
+    private redis: IORedis.Redis;
+    constructor() {
+        this.redis = new IORedis(process.env['REDIS_URI']);
+    }
+
+    async submitData(temperature: number, humidity: number) {
+        this.redis.connect();
+        await this.redis.xadd(process.env['ROOM'], '*', 'temparature', temperature, 'humidity', humidity);
+    }
+}
+
+
+async function main() {
+    const transmitter = new Transmitter();
+
+    const data = await Reader.readData();
+    await transmitter.submitData(data.temperature, data.humidity);
+}
+
+main();
