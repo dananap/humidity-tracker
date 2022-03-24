b2c943cd26c628492d8254b32eabe270213e5731
daniel <daniel@dpulm.online>
2022-03-24 14:22:36 +0100

    delete submodules

--- bin/instance.js
+++ bin/instance.js
@@ -7,6 +7,9 @@ require("dotenv/config");
 const os_1 = __importDefault(require("os"));
 const fs_1 = __importDefault(require("fs"));
 class Instance {
+    id;
+    room;
+    hostname;
     constructor() {
         this.id = fs_1.default.readFileSync('/etc/machine-id', 'utf8').trim();
         this.room = process.env['ROOM'];
@@ -14,6 +17,9 @@ class Instance {
     }
     get uptime() {
         return os_1.default.uptime();
+    }
+    get load() {
+        return os_1.default.loadavg()[0];
     }
     async getTemp() {
         const out = await fs_1.default.promises.readFile('/sys/class/thermal/thermal_zone0/temp', 'ascii');
